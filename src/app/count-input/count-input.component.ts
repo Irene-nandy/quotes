7e9ef4c6c119484376956c1ff6ca2b2132ef0f15
Irene-nandy <irenekasiva75@gmail.com>
2020-08-23 20:59:12 +0300

    add count.ts changes

--- src/app/count-input/count-input.component.ts
+++ src/app/count-input/count-input.component.ts
@@ -1,26 +1,26 @@
-// import { Component, OnInit, Input } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
-// @Component({
-//   selector: 'app-count-input',
-//   templateUrl: './count-input.component.html',
-//   styleUrls: ['./count-input.component.css']
-// })
-// export class CountInputComponent implements OnInit {
-//   @Input()
-// counterValue = 0;
-// counteValue = 0;
+@Component({
+  selector: 'app-count-input',
+  templateUrl: './count-input.component.html',
+  styleUrls: ['./count-input.component.css']
+})
+export class CountInputComponent implements OnInit {
+  @Input()
+counterValue = 0;
+counteValue = 0;
 
 
-// increment(){
-// this.counterValue ++;
-// }
+increment(){
+this.counterValue ++;
+}
 
-// decrement(){
-// this.counteValue ++;
-// } 
-//   constructor() { }
+decrement(){
+this.counteValue ++;
+} 
+  constructor() { }
 
-//   ngOnInit(): void {
-//   }
+  ngOnInit(): void {
+  }
 
-// }
+}
