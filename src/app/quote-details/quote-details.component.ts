52df2dfae2729d24726bf4127d84c6faedce4108
Irene-nandy <irenekasiva75@gmail.com>
2020-08-23 21:06:50 +0300

    add quote-details.component.ts changes

--- src/app/quote-details/quote-details.component.ts
+++ src/app/quote-details/quote-details.component.ts
@@ -1,24 +1,24 @@
-// import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
-// import {Quote} from '../quote'
-// @Component({
-//   selector: 'app-quote-details',
-//   templateUrl: './quote-details.component.html',
-//   styleUrls: ['./quote-details.component.css']
-// })
-// export class QuoteDetailsComponent implements OnInit {
-//   [x: string]: any;
+import { Component, OnInit, Input, Output,EventEmitter } from '@angular/core';
+import {Quote} from '../quote'
+@Component({
+  selector: 'app-quote-details',
+  templateUrl: './quote-details.component.html',
+  styleUrls: ['./quote-details.component.css']
+})
+export class QuoteDetailsComponent implements OnInit {
+  [x: string]: any;
   
-//   @Input() quote: Quote;
-//  upvote = 0;
-//  downvote = 0; 
-//  @Output() isComplete = new EventEmitter<boolean>();
-// quoteDelete(complete: boolean){
-// this.isComplete.emit(complete);
-// } 
+  @Input() quote: Quote;
+ upvote = 0;
+ downvote = 0; 
+ @Output() isComplete = new EventEmitter<boolean>();
+quoteDelete(complete: boolean){
+this.isComplete.emit(complete);
+} 
 
-//   constructor() { }
+  constructor() { }
 
-//   ngOnInit(): void {
-//   }
+  ngOnInit(): void {
+  }
 
-// }
+}
