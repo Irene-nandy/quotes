c332532b727ab9559ac1e64c22f01f97dfa00617
Irene-nandy <irenekasiva75@gmail.com>
2020-08-23 21:01:41 +0300

    add form.component.ts changes

--- src/app/form/form.component.ts
+++ src/app/form/form.component.ts
@@ -1,21 +1,21 @@
-// import { Component, OnInit, Output, EventEmitter } from '@angular/core';
-// import { Quote} from '../quote';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
+import { Quote} from '../quote';
 
-// @Component({
-//   selector: 'app-form',
-//   templateUrl: './form.component.html',
-//   styleUrls: ['./form.component.css']
-// })
-// export class FormComponent implements OnInit {
-//   newQuote = new Quote(0, "","","",new Date());
-//   @Output() addQuote = new EventEmitter<Quote>();
+@Component({
+  selector: 'app-form',
+  templateUrl: './form.component.html',
+  styleUrls: ['./form.component.css']
+})
+export class FormComponent implements OnInit {
+  newQuote = new Quote(0, "","","",new Date());
+  @Output() addQuote = new EventEmitter<Quote>();
 
-// submitQuote(){
-// this.addQuote.emit(this.newQuote);
-// } 
-//   constructor() { }
+submitQuote(){
+this.addQuote.emit(this.newQuote);
+} 
+  constructor() { }
 
-//   ngOnInit(): void {
-//   }
+  ngOnInit(): void {
+  }
 
-// }
+}
